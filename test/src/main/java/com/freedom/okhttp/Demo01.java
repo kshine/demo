fc3c62d141ade219b72ab3ec28715911fbdbb9f5
kshine <250270704@qq.com>
2017-04-15 20:42:38 +0800

    cmccopen 接口对接  todo response

--- test/src/main/java/com/freedom/okhttp/Demo01.java
+++ test/src/main/java/com/freedom/okhttp/Demo01.java
@@ -1,7 +1,10 @@
 package com.freedom.okhttp;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import com.freedom.util.HttpUtils;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by kuiyuxiang on 2017/4/14.
@@ -18,8 +21,40 @@ import okhttp3.Request;
  *  3b7f06fb3ec9991f
  */
 public class Demo01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        String appSign = "476affecfe1a48dc9c78b7f7ad01cb71";
+        String appKey = "da7e1886f2d07cbe";
+        Map<String,String> headersMap = HttpUtils.getHeaders(appSign,appKey);
+        String url = "http://211.138.118.14:1100/callshine/qryUserCondition/v1";
+        Headers headers =new Headers.Builder()
+//                .add("Host","aep.api.cmccopen.cn:1100")
+                        .add("Authorization",headersMap.get("Authorization"))
+                        .add("X-WSSE",headersMap.get("X-WSSE"))
+                        .add("Content-Type",headersMap.get("Content-Type"))
+                        .build();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("msisdn", "15168598761")
+                .add("subTitle","001")
+                .build();
+
         final OkHttpClient okHttpClient = new OkHttpClient();
-        Request request = new Request.Builder().build();
+        Request request = new Request.Builder()
+                .url(url)
+                .headers(headers)
+                .post(formBody)
+                .build();
+
+        Response response = okHttpClient.newCall(request).execute();
+        //code  description
+
+        System.out.println(response);
+        System.out.println(response.headers());
+        System.out.println(response.body().charStream().read());
+
+
+
+
     }
 }
