bd3045df52d945d5905b66f76af0c25d56126e8b
kshine <250270704@qq.com>
2017-05-21 18:04:10 +0800

    transaction

--- spring/src/main/java/com/freedom/transaction4/testApp4.java
+++ spring/src/main/java/com/freedom/transaction4/testApp4.java
@@ -1,16 +1,32 @@
 package com.freedom.transaction4;
 
 import com.freedom.transaction4.service.AccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by kshine on 2017/5/17.
+ * 整合Junit
+ * @RunWith(SpringJUnit4ClassRunner.class)
+ * @ContextConfiguration(locations = "classpath:transaction4.xml")
+ *  自动注入  @Autowired
+ *  @Test
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:transaction4.xml")
+
 public class testApp4 {
-    public static void main(String[] args) {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("transaction4.xml");
-        AccountService accountService=(AccountService) ac.getBean("accountService");
+
+    @Autowired
+     private AccountService accountService;
+
+    @Test
+     public void demo1(){
         accountService.transfer("tom","jack",100);
     }
 }
