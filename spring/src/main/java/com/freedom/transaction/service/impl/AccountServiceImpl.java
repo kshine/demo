d7261a2c06f71ddcee71868ac78b5b61eee17547
kshine <250270704@qq.com>
2017-05-21 15:25:48 +0800

    transaction

--- spring/src/main/java/com/freedom/transaction/service/impl/AccountServiceImpl.java
+++ spring/src/main/java/com/freedom/transaction/service/impl/AccountServiceImpl.java
@@ -2,6 +2,10 @@ package com.freedom.transaction.service.impl;
 
 import com.freedom.transaction.dao.AccountDao;
 import com.freedom.transaction.service.AccountService;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * Created by kshine on 2017/5/17.
@@ -14,9 +18,25 @@ public class AccountServiceImpl implements AccountService {
         this.accountDao = accountDao;
     }
 
+    private TransactionTemplate transactionTemplate;
+
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
     @Override
     public void transfer(String outer, String inner, Integer money) {
-        accountDao.out(outer,money);
-        accountDao.in(inner,money);
+
+        transactionTemplate.execute(
+                new TransactionCallbackWithoutResult() {
+                    @Override
+                    protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                        accountDao.out(outer,money);
+                       // int i = 1/0;
+                        accountDao.in(inner,money);
+                    }
+                }
+        );
+
     }
 }
